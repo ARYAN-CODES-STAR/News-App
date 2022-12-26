3b83023b21f8ac23e63b91c77300b565d5d0ebe3
ARYAN-CODES-STAR <aryansh0004@gmail.com>
2022-12-26 12:05:21 +0530

    Reduced json content

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -4,12 +4,22 @@ import Newsitem from './Newsitem'
 export class News extends Component {
   render() {
     return (
-      <div className="container my-3">
-        <p>this is a news component</p>
-        <Newsitem title="myTitle" description="hey one"/>
+      <div className="container my-4">
+        <h2>Taaza Khabar</h2>
+      <div className="row">
+        <div className="col-md-4">
+          <Newsitem title="myTitle" description="myone" imageUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJnSGmEJerJ5Wsi0y01slbS2HwNmxue82P2Ht0IZ_MFw&s"/>
+        </div>
+        <div className="col-md-4">
+          <Newsitem title="myTitle" description="myone"/>
+        </div>
+        <div className="col-md-4">
+          <Newsitem title="myTitle" description="myone"/>
+        </div>
       </div>
+   </div>
     )
   }
 }
 
-export default News
+export default News;
--- newsapp/src/components/Newsitem.js
+++ newsapp/src/components/Newsitem.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
 
 export class Newsitem extends Component {
+  constructor(){
+
+    
+  }
+ 
   render() {
-   let {title, description } = this.props;
+   let {title, description, imageUrl } = this.props;
     return (
-      <div>
+      <div className="my-3 ">
                     <div className="card" style={{width: "18rem"}}>
-              <img src="..." class="card-img-top" alt="..."/>
-              <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{description}</p>
-                <a href="/" class="btn btn-primary">Go somewhere</a>
-              </div>
+                    <img src={imageUrl} className="card-img-top" alt=""/>
+                    <div className="card-body">
+                      <h5 className="card-title">{title}</h5>
+                      <p className="card-text">{description}</p>
+                      <a href="/" className="btn btn-sm btn-primary">Read more</a>
+                    </div>
             </div>
       </div>
     )
